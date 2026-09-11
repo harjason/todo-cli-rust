1d7e74a415774630d52ab75b4abca5abb0db839c
harjason <nosajrarb2@gmail.com>
2026-09-11 17:34:35 +0530

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use std::fs;
+//use std::fs;
+use std::env;
 
 pub mod parser;
 
 fn main() -> std::io::Result<()> {
-    fs::create_dir("todos")?;
-    println!("welcome to todo app");
-    parser::parse();
+    //fs::create_dir("todos")?;
+
+    //env::args() returns an iterable, whose index 0 is irrelevant for our purposes 
+    let input: Vec<String> = env::args().collect(); 
+    let mut operation: String = String::new();
+
+    let mut args_vec: Vec<&String> = Vec::new(); 
+    
+    parser::parse(&input, &mut operation, &mut args_vec);
+
+    println!("input {:?}", input);
+    println!("operation: {}", operation);
+    println!("args: {:?}", args_vec);
+   // let input: parser::Command = parser::parse();
     Ok(())
 }
