21114ca7e7ac3c22bc169423ead7f3a96d781dbb
harjason <nosajrarb2@gmail.com>
2026-09-11 13:56:48 +0530

    add better parsing logic

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,26 @@
 use std::env;
 
-//pub struct Command(String, String, String);
+/// Tuple struct for command
+/// Example: todo create --high "finish logging"
+/// This will be constructed as Command("todo", vec!("create", "--high", "finish logging"))
+#[derive(Debug)]
+pub struct Command(String, Vec<String>);
 
-pub fn parse(){
-    let input: Vec<String> = env::args().collect();
-    dbg!(input);
+
+pub fn parse() -> Command { 
+
+    //env::args() returns an iterable, whose index 0 is irrelevant for our purposes 
+    let mut input: Vec<String> = env::args().collect();
+    let len = input.len();
+    
+    match len{
+        2_usize => Command(input.remove(1), vec!(String::new())),
+        3_usize => Command(input.remove(1), vec!(input.remove(1))),
+        4_usize => Command(input.remove(1), vec!(input.remove(1), input.remove(1))),
+
+    
+        //TODO-> add help funciton or something when > 2 args (excluding the first "todo" in the
+        //input")
+        0_usize..=1_usize | 5_usize.. => Command(String::new(), vec!(String::new())),
+    }
 }
