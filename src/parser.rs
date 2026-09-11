be07aecafbdef66ed852af5cb905bcb3dbc0c9a5
harjason <nosajrarb2@gmail.com>
2026-09-11 17:34:11 +0530

    update parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,26 +1,10 @@
-use std::env;
 
-/// Tuple struct for command
-/// Example: todo create --high "finish logging"
-/// This will be constructed as Command("todo", vec!("create", "--high", "finish logging"))
-#[derive(Debug)]
-pub struct Command(String, Vec<String>);
+pub fn parse<'a>(input: &'a Vec<String>, operation: &mut String, args_vec: &mut Vec<&'a String>) { 
 
-
-pub fn parse() -> Command { 
-
-    //env::args() returns an iterable, whose index 0 is irrelevant for our purposes 
-    let mut input: Vec<String> = env::args().collect();
     let len = input.len();
-    
-    match len{
-        2_usize => Command(input.remove(1), vec!(String::new())),
-        3_usize => Command(input.remove(1), vec!(input.remove(1))),
-        4_usize => Command(input.remove(1), vec!(input.remove(1), input.remove(1))),
 
-    
-        //TODO-> add help funciton or something when > 2 args (excluding the first "todo" in the
-        //input")
-        0_usize..=1_usize | 5_usize.. => Command(String::new(), vec!(String::new())),
-    }
+    operation.push_str(&input[1]);
+
+    if len > 2{ for i in 2..len{ args_vec.push(&input[i]); } }
+
 }
