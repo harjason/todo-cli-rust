a6e55c071eeb5d8db9a4edddc99218c5109d4ee6
harjason <nosajrarb2@gmail.com>
2026-09-12 15:24:08 +0530

    add todo dir creation code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-//use std::fs;
+use std::path::PathBuf;
 use std::env;
+use std::fs;
 
 pub mod parser;
+pub mod executor;
 
 fn main() -> std::io::Result<()> {
-    //fs::create_dir("todos")?;
+    let mut path = PathBuf::from("todos");
+    if !path.exists(){
+        fs::create_dir("todos")?;
+    }
 
     //env::args() returns an iterable, whose index 0 is irrelevant for our purposes 
     let input: Vec<String> = env::args().collect(); 
     let mut operation: String = String::new();
-
     let mut args_vec: Vec<&String> = Vec::new(); 
     
     parser::parse(&input, &mut operation, &mut args_vec);
+    executor::execute(&operation, &args_vec);
 
-    println!("input {:?}", input);
-    println!("operation: {}", operation);
-    println!("args: {:?}", args_vec);
-   // let input: parser::Command = parser::parse();
     Ok(())
 }
