7e6b348f4df484e4da3eb1fe1b75a889025c9ed6
harjason <nosajrarb2@gmail.com>
2026-09-12 17:17:38 +0530

    add executor functions

--- src/executor.rs
+++ src/executor.rs
@@ -1,12 +1,12 @@
 use std::fs::File;
+use std::fs;
 use std::path::PathBuf;
 use std::io::Result;
 
 pub fn execute(operation: &String, args: &Vec<&String>){
-    println!("created todo!");
     //create a todo list
     if operation == "create-list"{
-        if args.len() == 1{ let mut list =  create_list(args[0]);}
+        if args.len() == 1{ let _list =  create_list(args[0]);}
         else {
             println!("Wrong command");
             println!("Try \"todo create-list [listname]\" ");
@@ -15,18 +15,43 @@ pub fn execute(operation: &String, args: &Vec<&String>){
     //show all lists 
     else if operation == "lists"{
         if args.len() == 0{
-            show_all_lists();
+           let _ = show_all_lists();
         }
         else{
             println!("Invalid command");
             println!("Did you mean \"todo lists\" ?");
         }
     }
+    //show selected todo list
+    else if operation == "list"{
+        if args.len() == 0
+        {
+            let _ = show_selected_list();
+        }
+        else{
+            println!("Invalid command");
+            println!("Did you mean \"todo list\" ?");
+        }
+    }
+    //select a todo list
+    else if operation == "set"{
+        if args.len() == 1{
+           select_list(args[0]) 
+        }
+        else{
+            println!("Invalid command");
+            println!("Did you mean \"todo set [list name]\" ?");
+        }
+    }
+    else if operation == "add"{
+    }
     else{
         println!("Invalid command");
         println!("Try: todo --help");
     }
 }
+
+
 
 /// Create todo lists
 pub fn create_list(file_name: &String) -> Result<()>{
@@ -38,13 +63,61 @@ pub fn create_list(file_name: &String) -> Result<()>{
         println!("Todo list \"{}\" already exists", file_name);
     }
     else{
-    let mut file = File::create(&path)?;
+    let mut _file = File::create(&path)?;
+    let _ = fs::write(".current", file_name);
     println!("Todo list \"{}\" created!", file_name);
     }
     Ok(())
 }
 
 
+/// Show all existing todo lists
 pub fn show_all_lists(){
-    println!("showing all lists here");
+    let paths = fs::read_dir("todos").unwrap();
+    let mut total_lists = 0;
+    for path in paths{
+        total_lists += 1;
+        println!("{}", path.unwrap().file_name().display());
+    }
+    if total_lists == 0{
+        println!("No list exists");
+        println!("Create a list using \"todo create-list [listname]\"");
+    }
+    else{
+        println!("Total lists: {}" ,total_lists); 
+    }
+}
+
+/// Show selected list 
+pub fn show_selected_list(){
+    if let Ok(selected_list) = fs::read_to_string(".current"){
+        if selected_list == ""{
+            println!("No list selected");
+        }
+        else{
+            println!("{}", selected_list);
+        }
+    }
+}
+
+///Select a todo
+///the name of the selected todo list is stored in .current 
+pub fn select_list(list_name: &String) {
+    let mut does_exist = false;
+    let paths = fs::read_dir("todos").unwrap();
+    let list = format!("{}.md", list_name);
+    for path in paths{
+        if *list == *path.unwrap().file_name(){
+            does_exist = true;
+            break;
+        }
+    } 
+
+    if does_exist {
+        let _ = fs::write(".current", list_name);
+        println!("Selected todo list: {}", list_name);
+    }
+    else{
+        println!("Todolist \"{}\" does not exist.", list_name);
+    }
 }
