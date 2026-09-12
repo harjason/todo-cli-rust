03248bc8d012ccc15ac2a6d9c29703a7c0cbc24e
harjason <nosajrarb2@gmail.com>
2026-09-12 15:26:09 +0530

    update parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,7 @@
-
 pub fn parse<'a>(input: &'a Vec<String>, operation: &mut String, args_vec: &mut Vec<&'a String>) { 
-
     let len = input.len();
 
     operation.push_str(&input[1]);
-
+    // index[0] - value not needed, index [1] - commands, index[2] - args
     if len > 2{ for i in 2..len{ args_vec.push(&input[i]); } }
-
 }
